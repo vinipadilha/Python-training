90a54e62aebff1f72bbb60a3cb8afce745444824
Vinicius <viniciusdarosapadilha@gmail.com>
2025-09-06 18:38:54 -0300

    Importa classe Question e instancia objeto question

--- PythonFreeCode/quiz/app.py
+++ PythonFreeCode/quiz/app.py
@@ -1,4 +1,11 @@
+from Question import Question
+
 question_prompts = [
     "What is the capital of France?\n(a) Berlin\n(b) Madrid\n(c) Paris\n\n",
     "What is 2 + 2?\n(a) 3\n(b) 4\n(c) 5\n\n"
+]
+
+questions = [
+    Question(question_prompts[0], "c"),
+    Question(question_prompts[1], "b")  
 ]
