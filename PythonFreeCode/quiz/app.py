e4ba236d14bf11546abdb768a03fa5724f711e1d
Vinicius <viniciusdarosapadilha@gmail.com>
2025-09-06 18:48:23 -0300

    roda função e salva pontuação

--- PythonFreeCode/quiz/app.py
+++ PythonFreeCode/quiz/app.py
@@ -12,9 +12,13 @@ questions = [
 
 
 
+
 def run_test(questions):
     score = 0
     for question in questions: 
         answer = input(question.prompt)
         if answer.lower() == question.answer:
             score += 1
+    print("You got " + str(score) + "/" + str(len(questions)) + " correct")
+
+run_test(questions)
