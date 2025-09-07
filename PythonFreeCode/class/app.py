cd24b1ce314dfba6ca4cd35922d939a9b5fc0566
Vinicius <viniciusdarosapadilha@gmail.com>
2025-09-07 19:14:15 -0300

    Chama função criada na classe Student

--- PythonFreeCode/class/app.py
+++ PythonFreeCode/class/app.py
@@ -1,6 +1,8 @@
 from Student import Student
 
-student1 = Student("Jim", 20, "A")
+student1 = Student("Jim", 20, 3.6)
+student2 = Student("Jim", 20, 2)
 
-print(student1.name)
+print(student1.on_honor_roll())
+print(student2.on_honor_roll()) 
 
