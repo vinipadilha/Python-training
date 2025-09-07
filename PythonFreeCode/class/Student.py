4e086a4c04561b8e2cabf8dfa14fc06f810081d1
Vinicius <viniciusdarosapadilha@gmail.com>
2025-09-07 19:13:48 -0300

    Cria função dentro da classe Student

--- PythonFreeCode/class/Student.py
+++ PythonFreeCode/class/Student.py
@@ -1,5 +1,11 @@
 class Student:
-    def __init__ (self, name, age, grade) :
+    def __init__ (self, name, age, gpa):
         self.name = name
         self.age = age
-        self.grade = grade
+        self.gpa = gpa
+
+    def on_honor_roll(self):
+        if self.gpa >= 3.5:
+            return True
+        else:            
+            return False
