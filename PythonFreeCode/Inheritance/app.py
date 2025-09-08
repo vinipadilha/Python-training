4e7829db5f33154ccbdce5981d11bb197d4d694c
Vinicius <viniciusdarosapadilha@gmail.com>
2025-09-07 21:08:22 -0300

    Cria objeto de Chef generico e de chefe especifico

--- PythonFreeCode/Inheritance/app.py
+++ PythonFreeCode/Inheritance/app.py
@@ -1,4 +1,8 @@
 from Chef import Chef
+from ChineseChef import ChineseChef
 
 MyChef = Chef()
-MyChef.make_chicken()
+MyChef.make_special_dish()
+
+MyChineseChef = ChineseChef()
+MyChineseChef.make_special_dish()
