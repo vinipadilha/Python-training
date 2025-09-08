f32b9305d32c4ce34ecf25c5a6abde7f2f74429e
Vinicius <viniciusdarosapadilha@gmail.com>
2025-09-07 21:01:35 -0300

    Adiciona função que somente chef especifico faz

---
+++ PythonFreeCode/Inheritance/ChineseChef.py
@@ -0,0 +1,12 @@
+class ChineseChef:
+    def make_chicken(self):
+        print("The chef makes chicken")
+
+    def make_salad(self):
+        print("The chef makes salad")
+
+    def make_special_dish(self):
+        print("The chef makes orange chicken")
+
+    def make_fried_rice(self):
+        print("The chef makes fried rice")
