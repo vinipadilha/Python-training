4f231fc0e8c107055ba32374248d1431a426fc35
Vinicius <viniciusdarosapadilha@gmail.com>
2025-10-04 00:55:03 -0300

    feat: create db columns

--- Flask/FlaskMarket/market.py
+++ Flask/FlaskMarket/market.py
@@ -7,7 +7,11 @@ app = Flask(__name__)
 db = SQLAlchemy(app)
 
 class Item (db.Model):
-    name = db.Column(db.String(length=30), primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
 
 @app.route("/")
 @app.route("/home")
