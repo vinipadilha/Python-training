c46ec4aeae05c12ea55655fa99f9e995e8109c95
Vinicius <viniciusdarosapadilha@gmail.com>
2025-10-05 23:13:01 -0300

    chore: create market.db

--- Flask/FlaskMarket/market.py
+++ Flask/FlaskMarket/market.py
@@ -1,34 +1,37 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-
+import os
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db_path = os.path.join(app.root_path, 'market.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class Item (db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(length=30), nullable=False, unique=True)
-    price = db.Column(db.Integer(), nullable=False)
-    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
-    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+class Item(db.Model):
+    __tablename__ = 'item'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False, unique=True)
+    price = db.Column(db.Integer, nullable=False)
+    barcode = db.Column(db.String(12), nullable=False, unique=True)
+    description = db.Column(db.String(1024), nullable=False)
+
+    def __repr__(self):
+        return f'<Item {self.name}>'
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 @app.route("/home")
 def home_page():
     return render_template("home.html")
 
-
-@app.route('/market')
+@app.route("/market")
 def market_page():
-    items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-    ]
-    return render_template('market.html', items=items)
+    items = Item.query.all()
+    return render_template("market.html", items=items)
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
-
