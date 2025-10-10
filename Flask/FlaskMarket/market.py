3a00c4191d4a4ccc13265de2fd2fa931bcbd39d5
Vinicius <viniciusdarosapadilha@gmail.com>
2025-10-09 23:38:56 -0300

    chore: create routes file

--- Flask/FlaskMarket/market.py
+++ Flask/FlaskMarket/market.py
@@ -9,15 +9,5 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-@app.route("/")
-@app.route("/home")
-def home_page():
-    return render_template("home.html")
-
-@app.route("/market")
-def market_page():
-    items = Item.query.all()
-    return render_template("market.html", items=items)
-
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
