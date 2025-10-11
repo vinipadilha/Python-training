7e8bbd1fa9550d94245b7a4b27e6e0928a9de9f0
Vinicius <viniciusdarosapadilha@gmail.com>
2025-10-11 00:17:58 -0300

    feat: add User table

--- Flask/FlaskMarket/market/models.py
+++ Flask/FlaskMarket/market/models.py
@@ -1,5 +1,20 @@
 from market import db
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email_address = db.Column(db.String(50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(60), nullable=False)
+    budget = db.Column(db.Integer, nullable=False, default=1000)
+    items = db.relationship('Item', backref='owned_user', lazy=True)
+
+    @property
+    def prettier_budget(self):
+        return f'R$ {self.budget / 100:,.2f}'
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
 class Item(db.Model):
     __tablename__ = 'item'
     id = db.Column(db.Integer, primary_key=True)
--- Flask/FlaskMarket/run.py
+++ Flask/FlaskMarket/run.py
@@ -1,8 +1,7 @@
-from market import app
+from market import app, db
 
 if __name__ == "__main__":
+    # Cria as tabelas (se não existirem) dentro do contexto da aplicação
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, port=5001)
-
-
-
-    
